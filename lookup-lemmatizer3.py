ca2dd97b4a28ce3807e1021945dd528b6e330ad1
Ali-Omrani <aomrani@usc.edu>
2020-01-31 18:55:45 -0800

    fixed bugs, accuracies need work

--- lookup-lemmatizer3.py
+++ lookup-lemmatizer3.py
@@ -57,23 +57,25 @@ lemma_count = {}
 lemma_max = {}
 
 # Variables for reporting results
-training_stats = ['Wordform types' , 'Wordform tokens' , 'Unambiguous types' , 'Unambiguous tokens' , 'Ambiguous types' , 'Ambiguous tokens' , 'Ambiguous most common tokens' , 'Identity tokens']
-training_counts = dict.fromkeys(training_stats , 0)
+training_stats = ['Wordform types', 'Wordform tokens', 'Unambiguous types', 'Unambiguous tokens', 'Ambiguous types',
+                  'Ambiguous tokens', 'Ambiguous most common tokens', 'Identity tokens']
+training_counts = dict.fromkeys(training_stats, 0)
 
-test_outcomes = ['Total test items' , 'Found in lookup table' , 'Lookup match' , 'Lookup mismatch' , 'Not found in lookup table' , 'Identity match' , 'Identity mismatch']
-test_counts = dict.fromkeys(test_outcomes , 0)
+test_outcomes = ['Total test items', 'Found in lookup table', 'Lookup match', 'Lookup mismatch',
+                 'Not found in lookup table', 'Identity match', 'Identity mismatch']
+test_counts = dict.fromkeys(test_outcomes, 0)
 
 accuracies = {}
 
 ### Training: read training data and populate lemma counters
 
-train_data = open (train_file , 'r')
+train_data = open(train_file, 'r')
 
 train_form_token_count = 0
 for line in train_data:
-    
+
     # Tab character identifies lines containing tokens
-    if re.search ('\t' , line):
+    if re.search('\t', line):
 
         # Tokens represented as tab-separated fields
         field = line.strip().split('\t')
@@ -88,10 +90,10 @@ for line in train_data:
         if form in lemma_count:
             if lemma in lemma_count[form]:
                 lemma_count[form][lemma] += 1
-            else
+            else:
                 lemma_count[form][lemma] = 1
         else:
-            lemma_count[form] = {lemma:1}
+            lemma_count[form] = {lemma: 1}
 
         ######################################################
 
@@ -99,41 +101,41 @@ for line in train_data:
 max_lemma_count = 1
 for form in lemma_count.keys():
 
-        ######################################################
-        ### Insert code for building the lookup table      ###
-        form_lemma_counts = lemma_count[form]
-        max_count = 0
-        best_lemma = form
-        for lemma in form_lemma_counts:
-            if form_lemma_counts[lemma] > max_count:
-                max_count = form_lemma_counts[lemma]
-                best_lemma = lemma
+    ######################################################
+    ### Insert code for building the lookup table      ###
+    form_lemma_counts = lemma_count[form]
+    max_count = 0
+    best_lemma = form
+    for lemma in form_lemma_counts:
+        if form_lemma_counts[lemma] > max_count:
+            max_count = form_lemma_counts[lemma]
+            best_lemma = lemma
 
-        lemma_max[form] = lemma
-        ######################################################
+    lemma_max[form] = lemma
+    ######################################################
 
-        # training_stats = ['Wordform types', 'Wordform tokens', 'Unambiguous types', 'Unambiguous tokens',
-        #                'Ambiguous types', 'Ambiguous tokens', 'Ambiguous most common tokens', 'Identity tokens']
+    # training_stats = ['Wordform types', 'Wordform tokens', 'Unambiguous types', 'Unambiguous tokens',
+    #                'Ambiguous types', 'Ambiguous tokens', 'Ambiguous most common tokens', 'Identity tokens']
 
-        ######################################################
-        ### Insert code for populating the training counts ###
-        training_counts["Wordform types"] = len(lemma_count)
-        training_counts["Wordform tokens"] += sum(lemma_count[form].values())
+    ######################################################
+    ### Insert code for populating the training counts ###
+    training_counts["Wordform types"] = len(lemma_count)
+    training_counts["Wordform tokens"] += sum(lemma_count[form].values())
 
-        if len(lemma_count[form]) > 1:
-            training_counts["Ambiguous types"] += 1
-            training_counts["Ambiguous tokens"] += sum(lemma_count[form].values())
-            training_counts["Ambiguous most common tokens"] += max(lemma_count[form].values())
-        else:
-            training_counts["Unambiguous types"] +=1
-            training_counts["Unambiguous tokens"] += sum(lemma_count[form].values())
+    if len(lemma_count[form]) > 1:
+        training_counts["Ambiguous types"] += 1
+        training_counts["Ambiguous tokens"] += sum(lemma_count[form].values())
+        training_counts["Ambiguous most common tokens"] += max(lemma_count[form].values())
+    else:
+        training_counts["Unambiguous types"] += 1
+        training_counts["Unambiguous tokens"] += sum(lemma_count[form].values())
 
-        if form in lemma_count[form]:
-            training_counts["Identity tokens"] += lemma_count[form][form]
+    if form in lemma_count[form]:
+        training_counts["Identity tokens"] += lemma_count[form][form]
 
-        if len(lemma_count[form]) > max_lemma_count:
-            max_lemma_count = len(lemma_count[form])
-        ######################################################
+    if len(lemma_count[form]) > max_lemma_count:
+        max_lemma_count = len(lemma_count[form])
+    ######################################################
 ### Calculate expected accuracy if we used lookup on all items ###
 # Expected Lookup Accuracy = Ratio of the maximum count of the lemma tokens to the total wordform tokens
 
@@ -141,19 +143,18 @@ for form in lemma_count.keys():
 
 # Overall Accuracy = Ratio of total items found in the lookup table to the total test items
 
-accuracies['Expected lookup'] = max_lemma_count/training_counts["Wordform tokens"]
+accuracies['Expected lookup'] = max_lemma_count / training_counts["Wordform tokens"]
 
-
-accuracies['Expected identity'] = training_counts["Identitiy tokens"]/training_counts["Wordform tokens"]
+accuracies['Expected identity'] = training_counts["Identity tokens"] / training_counts["Wordform tokens"]
 
 ### Testing: read test data, and compare lemmatizer output to actual lemma
-    
-test_data = open (test_file , 'r')
+
+test_data = open(test_file, 'r')
 
 for line in test_data:
 
     # Tab character identifies lines containing tokens
-    if re.search ('\t' , line):
+    if re.search('\t', line):
 
         # Tokens represented as tab-separated fields
         field = line.strip().split('\t')
@@ -167,46 +168,46 @@ for line in test_data:
 
         # test_outcomes = ['Total test items', 'Found in lookup table', 'Lookup match', 'Lookup mismatch',
         #              'Not found in lookup table', 'Identity match', 'Identity mismatch']
-        test_outcomes["Total test items"] += 1
+        test_counts["Total test items"] += 1
         if form in lemma_max:
-            test_outcomes["Found in lookup table"] += 1
+            test_counts["Found in lookup table"] += 1
             if lemma_max[form] == lemma:
-                test_outcomes["Lookup match"] += 1
+                test_counts["Lookup match"] += 1
             else:
-                test_outcomes["Lookup mismatch"] += 1
+                test_counts["Lookup mismatch"] += 1
         else:
-            test_outcomes["Not found in lookup table"] += 1
+            test_counts["Not found in lookup table"] += 1
             if lemma == form:
-                test_outcomes["Identity match"] += 1
+                test_counts["Identity match"] += 1
             else:
-                test_outcomes["Identity mismatch"] += 1
+                test_counts["Identity mismatch"] += 1
 
         ######################################################
 ### Calculate accuracy on the items that used the lookup table ###
-accuracies['Lookup'] = test_outcomes["Lookup match"]/test_outcomes["Found in lookup table"]
+accuracies['Lookup'] = test_counts["Lookup match"] / test_counts["Found in lookup table"]
 
-accuracies['Identity'] = test_outcomes["Identity match"]/(test_outcomes["Not found in lookup table"])
+accuracies['Identity'] = test_counts["Identity match"] / (test_counts["Not found in lookup table"])
 ### Calculate overall accuracy ###
-accuracies['Overall'] = (test_outcomes["Lookup match"]+test_outcomes["Identity match"])/test_outcomes["Total test items"]
+accuracies['Overall'] = (test_counts["Lookup match"] + test_counts["Identity match"]) / test_counts["Total test items"]
 
 ### Report training statistics and test results
-                
-output = open ('lookup-output.txt' , 'w')
 
-output.write ('Training statistics\n')
+output = open('lookup-output.txt', 'w')
+
+output.write('Training statistics\n')
 
 for stat in training_stats:
-    output.write (stat + ': ' + str(training_counts[stat]) + '\n')
+    output.write(stat + ': ' + str(training_counts[stat]) + '\n')
 
-for model in ['Expected lookup' , 'Expected identity']:
-    output.write (model + ' accuracy: ' + str(accuracies[model]) + '\n')
+for model in ['Expected lookup', 'Expected identity']:
+    output.write(model + ' accuracy: ' + str(accuracies[model]) + '\n')
 
-output.write ('Test results\n')
+output.write('Test results\n')
 
 for outcome in test_outcomes:
-    output.write (outcome + ': ' + str(test_counts[outcome]) + '\n')
+    output.write(outcome + ': ' + str(test_counts[outcome]) + '\n')
 
-for model in ['Lookup' , 'Identity' , 'Overall']:
-    output.write (model + ' accuracy: ' + str(accuracies[model]) + '\n')
+for model in ['Lookup', 'Identity', 'Overall']:
+    output.write(model + ' accuracy: ' + str(accuracies[model]) + '\n')
 
 output.close

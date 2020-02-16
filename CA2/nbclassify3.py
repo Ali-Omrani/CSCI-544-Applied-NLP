636d1688fc5b36e7cb46564a00cecdbad0b99a10
Ali-Omrani <aomrani@usc.edu>
2020-02-16 10:13:20 -0800

    added a function to get read the texts

--- CA2/nbclassify3.py
+++ CA2/nbclassify3.py
@@ -18,17 +18,20 @@ negative_dir = [os.path.join(data_path, "negative_polarity")]
 truthful_dirs = [os.path.join(positive_dir[0], "truthful_from_TripAdvisor"), os.path.join(negative_dir[0], "truthful_from_TripAdvisor")]
 deceptive_dirs = [os.path.join(positive_dir[0], "deceptive_from_MTurk"), os.path.join(negative_dir[0], "deceptive_from_MTurk")]
 
-
+print(positive_dir)
 def get_file_paths(dir_paths):
-    files = []
+    text_files = []
     for dir_path in dir_paths:
+        print("exploring", dir_path)
         # r=root, d=directories, f = files
-        for r, d, f in os.walk(dir_path):
-            for file in f:
-                if '.txt' in file:
-                    files.append(os.path.join(r, file))
-    return files
+        for root, dirs, files in os.walk(dir_path):
+            for file in files:
+                if file.endswith(".txt"):
+                    if os.path.join(root, file) not in text_files:
+                        text_files.append(os.path.join(root, file))
+    return text_files
 
+category = "positive"
 file_paths_dict = {}
 
 file_paths_dict["positive"] = get_file_paths(positive_dir)
@@ -36,17 +39,22 @@ file_paths_dict["negative"] = get_file_paths(negative_dir)
 file_paths_dict["truthful"] = get_file_paths(truthful_dirs)
 file_paths_dict["deceptive"] = get_file_paths(deceptive_dirs)
 
-print(file_paths_dict["deceptive"])
+print(len(file_paths_dict[category]))
+# print(file_paths_dict["deceptive"])
 
-# print(type(h1))
-# h1 = [item for item in h1 if os.path.isdir(item)]
-# for item in h1:
-#     print(h1)
-#
-# h1 = []
-# for (dirpath_1, dirnames_1, filenames_1) in walk(data_path):
-#     h1.extend(dirnames_1)
-#     for (dirpath_2, dirname_2, filenames_2) in walk(h1):
-#         print(dirname_2)
-#     break
-# print(h1)
+
+def get_texts(file_paths):
+    texts = []
+    for file_path in file_paths:
+        # print("reading ", file_path)
+        file = open(file_path, mode='r')
+        all_of_it = file.read()
+        # print(all_of_it)
+        texts.append(all_of_it)
+    return texts
+
+texts = get_texts(file_paths_dict[category])
+
+
+print(len(texts))
+

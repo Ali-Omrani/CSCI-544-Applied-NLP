0e73b8336045561cf2949a01b2a7b810da6cfa30
Ali-Omrani <aomrani@usc.edu>
2020-02-17 09:46:59 -0800

    refactored file names

--- CA2/nbclassify3.py
+++ CA2/nbclassify3.py
@@ -1,18 +1,11 @@
-# this is token based classification
 import os
-from os import walk
-import csv
 import pickle
+import math
+import csv
 
-data_path = "data"
-test_fold_name = "fold1"
-
-positive_dir = [os.path.join(data_path, "positive_polarity")]
-negative_dir = [os.path.join(data_path, "negative_polarity")]
-
-truthful_dirs = [os.path.join(positive_dir[0], "truthful_from_TripAdvisor"), os.path.join(negative_dir[0], "truthful_from_TripAdvisor")]
-deceptive_dirs = [os.path.join(positive_dir[0], "deceptive_from_MTurk"), os.path.join(negative_dir[0], "deceptive_from_MTurk")]
-
+def read_probs(path_to_pickle):
+    with open(path_to_pickle, 'rb') as f:
+        return pickle.load(f)
 
 def get_text_file_paths(dir_paths):
     text_files = []
@@ -25,62 +18,56 @@ def get_text_file_paths(dir_paths):
                         text_files.append(os.path.join(root, file))
     return text_files
 
-file_paths_dict = {}
 
-file_paths_dict["positive"] = get_text_file_paths(positive_dir)
-file_paths_dict["negative"] = get_text_file_paths(negative_dir)
-file_paths_dict["truthful"] = get_text_file_paths(truthful_dirs)
-file_paths_dict["deceptive"] = get_text_file_paths(deceptive_dirs)
+def get_class_prob(file_path, class_probs):
+    file = open(file_path, mode='r')
+    text = file.read()
+    text = text.replace("\n", "").replace("(", "").replace(")", "").replace(".", "").replace(";", "").replace(",", "")
+    tokens = text.split(" ")
+    UK = 0
+    prob = 0
 
-
-
-def get_texts(file_paths):
-    texts = []
-    for file_path in file_paths:
-        # print("reading ", file_path)
-        file = open(file_path, mode='r')
-        all_of_it = file.read()
-        # print(all_of_it)
-        texts.append(all_of_it)
-    return texts
-
-
-
-def get_tokens(texts):
-    tokens = []
-    for text in texts:
-        text = text.replace("\n", "").replace("(", "").replace(")", "").replace(".", "").replace(";", "").replace(",",
-                                                                                                                  "")
-        tokens.extend(text.split(" "))
-    return tokens
-
-def get_bag_of_words(tokens):
-    bag_of_words = {}
     for token in tokens:
-        if token in bag_of_words:
-            bag_of_words[token] += 1
+        if token in class_probs:
+            prob += math.log(class_probs[token])
         else:
-            bag_of_words[token] = 1
+            UK += 1
 
-    return bag_of_words
+    return prob
 
-def get_probs(bag_of_words):
-    probs = {}
-    total = sum(bag_of_words.values())
-    for key in bag_of_words:
-        probs[key] = bag_of_words[key]/total
-    return probs
+# probs = {"ali": 0.2, "mamad": 0.3}
 
-
-def save_to_pickle(obj, filename):
-    with open(filename+'.pkl', 'wb') as f:
-        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+probs_paths = {"positive":"positive.pkl", "negative":"negative.pkl", "truthful": "truthful.pkl", "deceptive": "deceptive.pkl"}
+probs = {}
+for key in probs_paths:
+    probs[key] = read_probs(probs_paths[key])
 
 
-categories = ["positive", "negative", "truthful", "deceptive"]
-for category in categories:
-    texts = get_texts(file_paths_dict[category])
-    tokens = get_tokens(texts)
-    bag_of_words = get_bag_of_words(tokens)
-    pos_probs = get_probs(bag_of_words)
-    save_to_pickle(pos_probs, category)
+
+
+pred_dir_path = ["data/positive_polarity"]
+text_file_paths = get_text_file_paths(pred_dir_path)
+for pred_file_path in text_file_paths:
+    pos_prob = get_class_prob(pred_file_path, probs["positive"])
+    neg_prob = get_class_prob(pred_file_path, probs["negative"])
+    truthful_prob = get_class_prob(pred_file_path, probs["truthful"])
+    deceptive_prob = get_class_prob(pred_file_path, probs["deceptive"])
+
+    label1 = ""
+    label2 = ""
+
+    # print("p", positive_prob, "n", neg_prob, "t", truthful_prob, "d", deceptive_prob)
+
+
+    if pos_prob > neg_prob:
+        label1 = "positive"
+    else:
+        label1 = "negative"
+
+    if truthful_prob > deceptive_prob:
+        label2 = "truthful"
+    else:
+        label2 = "deceptive"
+
+    print(label1, label2, pred_file_path)
+
---
+++ CA2/nblearn3.py
@@ -0,0 +1,86 @@
+# this is token based classification
+import os
+from os import walk
+import csv
+import pickle
+
+data_path = "data"
+test_fold_name = "fold1"
+
+positive_dir = [os.path.join(data_path, "positive_polarity")]
+negative_dir = [os.path.join(data_path, "negative_polarity")]
+
+truthful_dirs = [os.path.join(positive_dir[0], "truthful_from_TripAdvisor"), os.path.join(negative_dir[0], "truthful_from_TripAdvisor")]
+deceptive_dirs = [os.path.join(positive_dir[0], "deceptive_from_MTurk"), os.path.join(negative_dir[0], "deceptive_from_MTurk")]
+
+
+def get_text_file_paths(dir_paths):
+    text_files = []
+    for dir_path in dir_paths:
+        # r=root, d=directories, f = files
+        for root, dirs, files in os.walk(dir_path):
+            for file in files:
+                if file.endswith(".txt"):
+                    if os.path.join(root, file) not in text_files:
+                        text_files.append(os.path.join(root, file))
+    return text_files
+
+file_paths_dict = {}
+
+file_paths_dict["positive"] = get_text_file_paths(positive_dir)
+file_paths_dict["negative"] = get_text_file_paths(negative_dir)
+file_paths_dict["truthful"] = get_text_file_paths(truthful_dirs)
+file_paths_dict["deceptive"] = get_text_file_paths(deceptive_dirs)
+
+
+
+def get_texts(file_paths):
+    texts = []
+    for file_path in file_paths:
+        # print("reading ", file_path)
+        file = open(file_path, mode='r')
+        all_of_it = file.read()
+        # print(all_of_it)
+        texts.append(all_of_it)
+    return texts
+
+
+
+def get_tokens(texts):
+    tokens = []
+    for text in texts:
+        text = text.replace("\n", "").replace("(", "").replace(")", "").replace(".", "").replace(";", "").replace(",",
+                                                                                                                  "")
+        tokens.extend(text.split(" "))
+    return tokens
+
+def get_bag_of_words(tokens):
+    bag_of_words = {}
+    for token in tokens:
+        if token in bag_of_words:
+            bag_of_words[token] += 1
+        else:
+            bag_of_words[token] = 1
+
+    return bag_of_words
+
+def get_probs(bag_of_words):
+    probs = {}
+    total = sum(bag_of_words.values())
+    for key in bag_of_words:
+        probs[key] = bag_of_words[key]/total
+    return probs
+
+
+def save_to_pickle(obj, filename):
+    with open(filename+'.pkl', 'wb') as f:
+        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+
+
+categories = ["positive", "negative", "truthful", "deceptive"]
+for category in categories:
+    texts = get_texts(file_paths_dict[category])
+    tokens = get_tokens(texts)
+    bag_of_words = get_bag_of_words(tokens)
+    pos_probs = get_probs(bag_of_words)
+    save_to_pickle(pos_probs, category)

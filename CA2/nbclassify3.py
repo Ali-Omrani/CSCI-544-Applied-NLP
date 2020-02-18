e115680c064044550dd27ae39ddd1fbec03074e5
Ali-Omrani <aomrani@usc.edu>
2020-02-17 17:21:41 -0800

    laplace smoothing implemented in classify

--- CA2/nbclassify3.py
+++ CA2/nbclassify3.py
@@ -8,7 +8,7 @@ import re
 STOP_WORDS = ["i","me","my","myself","we","our","ours","ourselves","you","your","yours","yourself","yourselves","he","him","his","himself","she","her","hers","herself","it","its","itself","they","them","their","theirs","themselves","what","which","who","whom","this","that","these","those","am","is","are","was","were","be","been","being","have","has","had","having","do","does","did","doing","a","an","the","and","but","if","or","because","as","until","while","of","at","by","for","with","about","against","between","into","through","during","before","after","above","below","to","from","up","down","in","out","on","off","over","under","again","further","then","once","here","there","when","where","why","how","all","any","both","each","few","more","most","other","some","such","no","nor","not","only","own","same","so","than","too","very","s","t","can","will","just","don","should","now"]
 
 
-def read_probs(path_to_pickle):
+def read_counts(path_to_pickle):
     with open(path_to_pickle, 'rb') as f:
         return pickle.load(f)
 
@@ -24,7 +24,7 @@ def get_text_file_paths(dir_paths):
     return text_files
 
 
-def get_class_prob(file_path, class_probs, vocab):
+def get_class_prob(file_path, class_counts, vocab):
     file = open(file_path, mode='r')
     text = file.read()
     translator = str.maketrans("", "", string.punctuation)
@@ -35,37 +35,37 @@ def get_class_prob(file_path, class_probs, vocab):
             tokens.append(item)
     UK = 0
     prob = 0
-
+    total = sum(class_counts.values())
     for token in tokens:
         if token not in vocab:
             continue
-        if token in class_probs:
-            prob += math.log(class_probs[token])
+        if token in class_counts:
+            prob += math.log((class_counts[token] + 1)/(total + len(vocab)))
         else:
-            prob += math.log(1/len(vocab))
+            prob += math.log(1/(total+len(vocab)))
 
     return prob
 
-# probs = {"ali": 0.2, "mamad": 0.3}
+
 
 vocab = []
 with open("vocab.pkl", 'rb') as f:
     vocab = pickle.load(f)
 
-probs_paths = {"positive":"positive.pkl", "negative":"negative.pkl", "truthful": "truthful.pkl", "deceptive": "deceptive.pkl"}
-probs = {}
-for key in probs_paths:
-    probs[key] = read_probs(probs_paths[key])
+counts_paths = {"positive":"positive.pkl", "negative":"negative.pkl", "truthful": "truthful.pkl", "deceptive": "deceptive.pkl"}
+counts = {}
+for key in counts_paths:
+    counts[key] = read_counts(counts_paths[key])
 
 pred_dir_path = [sys.argv[1]]
 # pred_dir_path = ["data/positive_polarity"]
 text_file_paths = get_text_file_paths(pred_dir_path)
 outF = open("nboutput.txt", "w")
 for pred_file_path in text_file_paths[1:]:
-    pos_prob = get_class_prob(pred_file_path, probs["positive"],vocab)
-    neg_prob = get_class_prob(pred_file_path, probs["negative"], vocab)
-    truthful_prob = get_class_prob(pred_file_path, probs["truthful"], vocab)
-    deceptive_prob = get_class_prob(pred_file_path, probs["deceptive"], vocab)
+    pos_prob = get_class_prob(pred_file_path, counts["positive"],vocab)
+    neg_prob = get_class_prob(pred_file_path, counts["negative"], vocab)
+    truthful_prob = get_class_prob(pred_file_path, counts["truthful"], vocab)
+    deceptive_prob = get_class_prob(pred_file_path, counts["deceptive"], vocab)
 
     label1 = ""
     label2 = ""

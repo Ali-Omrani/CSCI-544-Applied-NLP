670dc6f7d72fa26a335eed1956cb396c15d34c85
Ali-Omrani <aomrani@usc.edu>
2020-04-05 15:21:55 -0700

    build A , Pi , tag_count

--- CA4/hmmlearn.py
+++ CA4/hmmlearn.py
@@ -8,6 +8,7 @@ import re
 data_path = sys.argv[1]
 # data_path = "data/hmm-training-data/it_isdt_train_tagged.txt"
 
+tag_count_dict = dict()
 A = dict()
 B = dict()
 Pi = dict ()
@@ -22,4 +23,32 @@ for line in Lines:
     tokens = line.split(" ")
     print(line)
     print(tokens)
+    prev_tag = False
+    for token in tokens:
+        splitted_token = token.rsplit("/", 1)
+        tag = splitted_token[1]
+        word = splitted_token[0]
+
+        if tag not in tag_count_dict:
+            tag_count_dict[tag] = 1
+        else:
+            tag_count_dict[tag] += 1
+
+        if not prev_tag:
+            if tag not in Pi:
+                Pi[tag] = 1
+            else:
+                Pi[tag] += 1
+        else:
+            if tag not in A:
+                A[tag] = {prev_tag : 1}
+            else:
+                if prev_tag not in A[tag]:
+                    A[tag][prev_tag] = 1
+                else:
+                    A[tag][prev_tag] += 1
+
+
+
+        prev_tag = tag
     print("-------------------")

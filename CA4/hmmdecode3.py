df29507a9dd2584abd45dadeea3c0e4c8d90cffd
Ali-Omrani <aomrani@usc.edu>
2020-04-05 17:17:04 -0700

    reading probs

--- CA4/hmmdecode3.py
+++ CA4/hmmdecode3.py
@@ -3,3 +3,18 @@ import pickle
 import sys
 import string
 import re
+
+path_to_pickle = "dict.pickle"
+
+
+with open(path_to_pickle, 'rb') as f:
+    probs = pickle.load(f)
+
+
+    A = probs["A"]
+    B = probs["B"]
+    Pi = probs["Pi"]
+
+    print(A)
+    print(B)
+    print(Pi)
